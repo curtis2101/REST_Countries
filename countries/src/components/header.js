51bd5dc52ca022b5c7da8c7e56989755c8d58722
Curtis Sanderson <cw1sander@gmail.com>
2022-08-13 23:17:21 +0200

    aligned header, added input search field and drop down box

--- countries/src/components/header.js
+++ countries/src/components/header.js
@@ -4,8 +4,8 @@ import {HiOutlineMoon} from 'react-icons/hi'
 const Header = () => {
     return ( 
         <div className="border-b-2 shadow-sm flex justify-between items-center w-full h-20 bg-white">
-            <h1 className="ml-4 font-nunitoSans text-sm font-extrabold">Where in the world?</h1>
-            <h2 className="inline-flex mr-4 font-medium text-xs font-nuntioSans"><HiOutlineMoon size={20}/> Dark Mode</h2>
+            <h1 className="ml-4 text-sm font-extrabold">Where in the world?</h1>
+            <h2 className="inline-flex w-24 items-end mb-1 justify-between mr-4 font-semibold text-xs"><HiOutlineMoon size={20}/> Dark Mode</h2>
         </div>
     );
 }
--- countries/src/pages/home.js
+++ countries/src/pages/home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Header from '../components/header.js'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const Home = () => {
     return ( 
         <main className='h-screen flex flex-col items-center w-full overflow-x-hidden bg-verylightgrey overflow-scroll"'>
             <Header />
+            <div className='flex flex-col items-center w-full'>
+                <div className='relative flex text-gray-400 w-full'>
+                    <AiOutlineSearch className='absolute left-12 bottom-4 text-xl text-gray-400'/>
+                    <input type='text' placeholder='Search for a country...' className='pr-3 pl-20 py-2 ml-4 font-normal text-xs rounded mt-6 shadow-md h-12 w-11/12'></input>
+                </div>
+                <section className='flex w-full ml-4 mt-10'>
+                    <select className='h-12 w-6/12 pl-6 rounded shadow-md font-normal text-xs'>
+                        <option value="" disabled selected hidden>Filter by Region</option>
+                        <option value="fake">Fake</option>
+                    </select>
+                </section>
+            </div>
 
 
 
