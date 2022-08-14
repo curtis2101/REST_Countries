344e909ee2b49f3c1913d38cb6e969bb568f72c9
Curtis Sanderson <cw1sander@gmail.com>
2022-08-14 15:49:09 +0200

    added span and font weight and sizing to country cards

--- countries/src/pages/home.js
+++ countries/src/pages/home.js
@@ -8,8 +8,9 @@ const Home = () => {
 
     return ( 
         <main className='h-screen flex flex-col items-center w-full overflow-x-hidden bg-verylightgrey overflow-scroll"'>
-            <Header />
+            
             <div className='flex flex-col items-center w-full'>
+            <Header />
                 <div className='relative flex text-gray-400 w-full'>
                     <AiOutlineSearch className='absolute left-12 bottom-4 text-xl text-gray-400'/>
                     <input type='text' placeholder='Search for a country...' className='pr-3 pl-20 py-2 ml-4 font-normal text-xs rounded mt-6 shadow-md h-12 w-11/12'></input>
@@ -27,10 +28,10 @@ const Home = () => {
                     <section className='mt-10 flex flex-col items-center'>
                         <div className='rounded-lg bg-white w-9/12 h-80 shadow-md'>
                             <img className="rounded-t-lg w-full h-36"src={data.flags.png} alt="flag"/>
-                            <h1 className="mt-4 ml-4 font-bold">{data.name}</h1>
-                            <h2 className="mt-4 ml-4">Population: {data.population}</h2>
-                            <h2 className="mt-4 ml-4">Region: {data.region}</h2>
-                            <h2 className="mt-4 ml-4">Capital: {data.capital}</h2>
+                            <h1 className="mt-4 ml-4 font-extrabold text-lg">{data.name}</h1>
+                            <h2 className="mt-4 ml-4 font-semibold text-sm">Population: <span className="text-sm font-light">{data.population}</span></h2>
+                            <h2 className="mt-4 ml-4 font-semibold text-sm">Region: <span className='text-sm font-light'>{data.region}</span></h2>
+                            <h2 className="mt-4 ml-4 font-semibold text-sm">Capital: <span className='text-sm font-light'>{data.capital}</span></h2>
                         </div>
                     </section>
                 ))}
