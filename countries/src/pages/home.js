dbb1a825184cbd321902408b90d627a5a5137619
Curtis Sanderson <cw1sander@gmail.com>
2022-08-15 19:13:10 +0200

    changed filter method

--- countries/src/pages/home.js
+++ countries/src/pages/home.js
@@ -1,16 +1,29 @@
 import Header from '../components/header.js'
 import {AiOutlineSearch} from 'react-icons/ai'
-import AllCountries from '../components/allCountries.js'
-import Africa from '../components/africa.js'
-import America from '../components/america.js'
-import Asia from '../components/asia.js'
-import Europe from '../components/europe.js'
-import Oceania from '../components/oceania.js'
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
+
 
 const Home = () => {
-    const [region,setRegion] = useState("");
+    const [countries, setCountries] = useState([]);
+    const [showh,setShowh] = useState(true);
     
+    
+    useEffect( () => {
+        async function fetchData(){
+        const res = await fetch('https://restcountries.com/v2/all')
+        const data = await res.json()
+        await setCountries(data)}
+        fetchData()
+    }, [])
+
+    const filterByRegion = async region => {
+        if(region === '') return
+        const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+        const data = await res.json()
+        await setCountries(data)
+        setShowh(false)
+        
+    }
 
     return ( 
         <main className='h-screen flex flex-col items-center w-full overflow-x-hidden bg-verylightgrey overflow-scroll"'>
@@ -21,10 +34,7 @@ const Home = () => {
                     <input type='text' placeholder='Search for a country...' className='pr-3 pl-20 py-2 ml-4 font-normal text-xs rounded mt-6 shadow-md h-12 w-11/12'></input>
                 </div>
                 <section className='flex w-full ml-8 mt-10'>
-                    <select className='h-12 w-6/12 pl-6 rounded shadow-md font-normal text-xs' onChange={(event) => {
-                        const selectedRegion = event.target.value;
-                        setRegion(selectedRegion);
-                    }}>
+                    <select className='h-12 w-6/12 pl-6 rounded shadow-md font-normal text-xs' onChange={ e => filterByRegion(e.target.value)}>
                     <option value="" disabled selected hidden>Filter by Region</option>
                     <option value="Africa">Africa</option>
                     <option value="America">America</option>
@@ -33,13 +43,22 @@ const Home = () => {
                     <option value="Oceania">Oceania</option>
             </select>
         </section>
-                {region === "" ? <AllCountries /> : null}
-                {region === "Africa"? <Africa/>:null}
-                {region === "America"? <America/>:null}
-                {region === "Asia"? <Asia/>:null}
-                {region === "Europe"? <Europe/>:null}
-                {region === "Oceania"? <Oceania/>:null}
             </div>
+            <div className="flex flex-col w-full mt-10">
+        {countries.map((country,index) => (
+            <section key={index} className='mb-10 flex flex-col items-center'>
+                <div className='rounded-lg bg-white w-9/12 h-80 shadow-md'>
+                    <img className="rounded-t-lg w-full h-36"src={country.flags.png} alt="flag"/>
+                    {showh ?<h1 className="mt-4 ml-4 font-extrabold text-lg">{country.name}</h1>:<h1 className="mt-4 ml-4 font-extrabold text-lg">{country.name.common}</h1>}
+                    <h2 className="mt-4 ml-4 font-light text-sm"><span className="font-semibold">Population:</span> {country.population}</h2>
+                    <h2 className="mt-4 ml-4 font-light text-sm"><span className="font-semibold">Region:</span> {country.region}</h2>
+                    <h2 className="mt-4 ml-4 font-light text-sm"><span className="font-semibold">Capital:</span> {country.capital}</h2>
+                </div>
+            </section>
+        ))}
+
+
+        </div>
 
 
 
