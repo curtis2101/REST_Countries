c81b0b65d4e9d5729f3292519ef8e5cac8c1cde8
Curtis Sanderson <cw1sander@gmail.com>
2022-08-18 18:45:59 +0200

    added native name

--- countries/src/pages/country.js
+++ countries/src/pages/country.js
@@ -9,6 +9,7 @@ const Country = () => {
     const [details,setDetails] = useState(null);
     const [string,setString] = useState(null);
     const [lang , setLang] = useState(null);
+    const [native, setNative] = useState(null);
 
 
     
@@ -22,6 +23,7 @@ const Country = () => {
         (data => {setDetails(data);
             setString(Object.keys(data[0].currencies || {}).toString());
             setLang(Object.keys(data[0].languages || {}).toString());
+            setNative(Object.keys(data[0].name.nativeName || {}).toString());
         })
     }, [country])
         
@@ -33,12 +35,12 @@ const Country = () => {
                 <section className='flex justify-items-start w-full'>
                     <button className='border-2 ml-7 mt-10 mb-16 rounded shadow-md hover:shadow-inner cursor-pointer w-3/12 flex flex-row items-center justify-center text-sm font-light'><span className='mr-2 text-lg'><BsArrowLeft/></span>Back</button>
                 </section>
-                {details && string && lang && details.map((data,index) => 
+                {details && details.map((data,index) => 
                 <div className='w-full flex flex-col items-center' key={index}>
                     <img className='rounded mb-11 w-10/12 h-6/6' alt={data.name.common} src={data.flags.png}/>
                     <section className='flex flex-col w-full '>
                         <h1 className='font-extrabold text-xl ml-7'>{data.name.common}</h1>
-                        <p className='ml-7 mt-4 text-sm font-light'><span className='text-sm font-semibold'>Official Name: </span>{data.name.official}</p>
+                        <p className='ml-7 mt-4 text-sm font-light'><span className='text-sm font-semibold'>Native Name: </span>{data.name.nativeName[native].common}</p>
                         <p className='ml-7 mt-2 text-sm font-light'><span className='text-sm font-semibold'>Population: </span>{data.population}</p>
                         <p className='ml-7 mt-2 text-sm font-light'><span className='text-sm font-semibold'>Region: </span>{data.region}</p>
                         <p className='ml-7 mt-2 text-sm font-light'><span className='text-sm font-semibold'>Sub Region: </span>{data.subregion}</p>
